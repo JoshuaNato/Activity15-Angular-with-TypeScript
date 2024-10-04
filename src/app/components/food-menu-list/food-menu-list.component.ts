3e9c2f2cbea1be48c22e24ca8b354228f569de97
JoshuaNato <natojoshua22@gmail.com>
2024-10-04 19:57:09 +0800

    Added component #21: Food Menu List

---
+++ src/app/components/food-menu-list/food-menu-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-menu-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-menu-list.component.html',
+  styleUrl: './food-menu-list.component.css'
+})
+export class FoodMenuListComponent {
+
+}
