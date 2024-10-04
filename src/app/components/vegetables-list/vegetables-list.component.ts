80ce2623b3e97c1ec70c568bc072fbc701386bf4
JoshuaNato <natojoshua22@gmail.com>
2024-10-04 19:51:35 +0800

    Added component #13: Vegetables List

---
+++ src/app/components/vegetables-list/vegetables-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vegetables-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './vegetables-list.component.html',
+  styleUrl: './vegetables-list.component.css'
+})
+export class VegetablesListComponent {
+
+}
